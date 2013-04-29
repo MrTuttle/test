3b7803482c38092c58e9c608e582aee6237495ff
Florent <zeflo74@gmail.com>
2013-04-29 21:45:09 +0200

    add some styles

--- flog/wp-content/themes/twentytwelve/header.php
+++ flog/wp-content/themes/twentytwelve/header.php
@@ -29,7 +29,7 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
-<link rel="stylesheet" href="flog.css">
+
 </head>
 
 <body <?php body_class(); ?>>
