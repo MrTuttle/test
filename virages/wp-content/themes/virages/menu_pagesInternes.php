3225442d6d4c9e3ea0aed07784b2f4ccac6f757c
Florent <zeflo74@gmail.com>
2013-04-29 21:12:01 +0200

    delete old wordpress, replace by another

--- virages/wp-content/themes/virages/menu_pagesInternes.php
+++
@@ -1,6 +0,0 @@
-<ul class="unstyled" id="ssMenu_pagesInternes">
-	<li class="active"><a href="">FORMATIONS</a></li>
-	<li><a href="http://virages.agencealbum.com/formations/">VOIR LE PARCOURS</a></li>
-	<li><a href="">FINANCEMENT</a></li>
-	<li><a href="http://virages.agencealbum.com/inscription/">INSCRIPTION</a></li>
-</ul>
--- virages/wp-content/themes/virages/newsletter.php
+++
@@ -1,1 +0,0 @@
-<p class="text-center" id="suivez-nous">Suivre notre actualité ? <a href="">Inscrivez-vous à la newsletter ></a></p>
